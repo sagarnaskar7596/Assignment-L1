7233e7da1a4b0fccdee2fa822d22d2a4688caadb
sagarnaskar7596 <145820442+sagarnaskar7596@users.noreply.github.com>
2023-09-23 08:44:03 +0530

    Answer of Assignment L1

---
+++ Assignment  L1-Output & Variable/circumference_of_a_circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    int r=10;
+    cout<<"Radius of the circle "<<r<<endl;
+    float C=2*3.14*r;
+    cout<<"Cicumference of the circle "<< C ;
+}
